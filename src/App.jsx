f19ba2ea1761d9ba29ef310f7539e6d0cb25488f
Tciganskybaron <Tihonov.n.01@gmail.com>
2024-07-29 21:17:36 +0200

    Start

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { Button } from './Component/Button/Button';
+import { Chart } from './Component/Chart/Chart';
+
+const initialData = [
+    { time: '2018-12-22', value: 32.51 },
+    { time: '2018-12-23', value: 31.11 },
+    { time: '2018-12-24', value: 27.02 },
+    { time: '2018-12-25', value: 27.32 },
+    { time: '2018-12-26', value: 25.17 },
+    { time: '2018-12-27', value: 28.89 },
+    { time: '2018-12-28', value: 25.46 },
+    { time: '2018-12-29', value: 23.92 },
+    { time: '2018-12-30', value: 22.68 },
+    { time: '2018-12-31', value: 22.67 },
+];
 
 export default function App() {
   return (
     <div>
-      <h1>React Parcel Starter Kit</h1>
+			<Chart data={initialData} /> 
     </div>
   );
 }
