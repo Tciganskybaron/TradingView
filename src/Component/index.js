b90b52af0a7ea23d340353a5f8b9a6cbfb4e0ec9
Tciganskybaron <Tihonov.n.01@gmail.com>
2024-07-30 16:28:31 +0200

    deleted Component

--- src/Component/index.js
+++
@@ -1,5 +0,0 @@
-export { Chart } from './Chart/Chart';
-export { RangeSwitcher } from './RangeSwitcher/RangeSwitcher';
-export { CoinSwitcher } from './CoinSwitcher/CoinSwitcher';
-export { ButtonGroup } from './ButtonGroup/ButtonGroup';
-export { ChartSwitcher } from './ChartSwitcher/ChartSwitcher';
