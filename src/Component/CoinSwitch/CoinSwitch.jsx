5e25a6edaf3c5b40ad0be61acc37e63d8db0990d
Tciganskybaron <Tihonov.n.01@gmail.com>
2024-07-30 12:39:20 +0200

    CoinSwitch styles

--- src/Component/CoinSwitch/CoinSwitch.jsx
+++ src/Component/CoinSwitch/CoinSwitch.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './CoinSwitch.module.css';
+import cn from 'classnames';
 
 const COINS = [
 	{ label: 'Solana', value: 'SOLUSDT' },
@@ -8,10 +10,23 @@ const COINS = [
 
 export function CoinSwitch(props) {
 	const { setCoin } = props;
+	const [selectedCoin, setSelectedCoin] = useState('SOLUSDT');
+
+	const handleCoinChange = (value) => {
+		setSelectedCoin(value);
+		setCoin(value);
+	};
+
 	return (
-		<div>
+		<div className={styles.buttonGroup}>
 			{COINS.map(coinOption => (
-				<button key={coinOption.value} onClick={() => setCoin(coinOption.value)}>
+				<button
+					key={coinOption.value}
+					className={cn(styles.button, {
+						[styles.active]: selectedCoin === coinOption.value,
+					})}
+					onClick={() => handleCoinChange(coinOption.value)}
+				>
 					{coinOption.label}
 				</button>
 			))}
